26db04b97f0602c68f09fe4b918dd144f29b3151
EkoLabs Samples <46129036+EkoLabsSamples@users.noreply.github.com>
2019-10-16 16:38:36 +0200

    Update app.js
    Updated Lottie configuration according to here: https://developer.helloeko.com/docs/howto/svgs_and_lottie.html#Using-Lottie-Elements

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,10 @@ import buttonAnimation from "./chug.json";
 import timerAnimation from "./timer.json";
 import Lottie from 'lottie-web';
 
+// Configure Lottie. You need to do this only once. Read more here:
+// https://developer.helloeko.com/docs/howto/svgs_and_lottie.html#Using-Lottie-Elements
+Lottie.setLocationHref(window.location.href);
+
 export default {
     onLoad: function(ctx) { },
 
